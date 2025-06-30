91abb6330ff21d059ad84fd9c2ba97b59e8142ae
ankon456 <ankonmukharjee@gmail.com>
2025-06-30 20:01:29 +0600

    Create circle_area.c
    Finding Area of a circle.This is practice 10.

---
+++ circle_area.c
@@ -0,0 +1,16 @@
+/*
+Finding Area of a circle 
+circle_area.c
+practice10.c
+*/
+
+#include <stdio.h> 
+int main()
+{
+    float radius, area;
+    printf("Enter radius of the circle (in cm) : ");
+    scanf("%f", &radius);
+    area= (22/7)*(radius*radius);
+    printf("Area of the circle is : %f square cm", area);
+    return 0;
+}
